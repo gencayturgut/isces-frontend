7f5d054ea140a254faf581d4f00b83306dd727b3
Gencay Turgut <gencayturgut2@gmail.com>
2023-06-09 00:32:32 +0300

    a

--- ui/src/StudentPages/Council.js
+++ ui/src/StudentPages/Council.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export default function Council() {
   const [delegates, setDelegates] = useState([]);
-  const url = "https://iztechelectionfrontend.herokuapp.com//allDelegates";
+  const url = "https://iztechelectionfrontend.herokuapp.com/allDelegates";
 
   const fetchDelegates = async () => {
     try {
